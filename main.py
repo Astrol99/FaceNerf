32b2f372edbd1137e640f4cb398af849d8742122
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2020-07-19 16:06:13 -0500

    Implemented working face detection using haarcascade

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import face_recognition
 import cv2
 import numpy as np
+
+# Load Cascades
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 cap = cv2.VideoCapture(0)
 # Better FPS and processing
 cap.set(3, 640)
 cap.set(4, 480)
 
-while True:
-    ret,frame = cap.read()
+while cap.isOpened():
+    ret, frame = cap.read()
 
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('img1', frame)
+    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert frame to grayscale
+
+    faces = faceCascade.detectMultiScale(frameGray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        x2, y2 = x+w, y+h
+        frame = cv2.rectangle(frame, (x,y), (x2, y2), (0,0,255), 1)
+
+    cv2.imshow('Video Capture', frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
