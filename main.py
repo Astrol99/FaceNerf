cb4208114f048c30409ef273270b7443d1f5ab68
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2020-07-22 13:53:23 -0500

    Added target prioritization based on closest face -> face area

--- main.py
+++ main.py
@@ -12,13 +12,27 @@ cap.set(4, 480)
 while cap.isOpened():
     ret, frame = cap.read()
 
-    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert frame to grayscale
+    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(frameGray, 1.3, 5)
+    faceClosest = {
+        "area": 0,
+        "xy1": (0,0),
+        "xy2": (0,0)
+    }
+
     for (x, y, w, h) in faces:
         x2, y2 = x+w, y+h
-        frame = cv2.rectangle(frame, (x,y), (x2, y2), (0,0,255), 1)
+        faceArea = w * h
 
+        frame = cv2.rectangle(frame, (x,y), (x2,y2), (0,255,0), 1)
+
+        if faceArea > faceClosest["area"]:
+            faceClosest["area"] = faceArea
+            faceClosest["xy1"] = (x,y)
+            faceClosest["xy2"] = (x2,y2)
+
+    frame = cv2.rectangle(frame, faceClosest["xy1"], faceClosest["xy2"], (0,0,255), 1)
     cv2.imshow('Video Capture', frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
