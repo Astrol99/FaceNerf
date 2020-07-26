a31808e78c0159de4bc2f78b73886eb8accdc009
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2020-07-26 16:51:01 -0500

    Stop and idle signals not needed

--- interface.py
+++ interface.py
@@ -8,23 +8,17 @@ arduinoData = serial.Serial("/dev/ttyACM0",9600)
 L  | LEFT
 F  | FIRE
 R  | RIGHT
-S  | STOP (Sends signal to stop repeating signal sent before)
 """
 
+# Continously move in to 
 def left():
     print("LEFT")
     arduinoData.write(b'L')
-
-    arduinoData.write(b'S')
 
 def fire():
     print("FIRING")
     arduinoData.write(b'F')
 
-    arduinoData.write(b'S')
-
 def right():
     print("RIGHT")
     arduinoData.write(b'R')
-
-    arduinoData.write(b'S')
