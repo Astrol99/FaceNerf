d5b5925a0b5ddb778ebbf0651048683eda4a679c
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2020-07-26 17:27:53 -0500

    Fully Functional Servo Controller from Serial Connection

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,27 +1,47 @@
+#include <Servo.h>
+
 char serialData;
-int pin = 13;
+Servo servoX;
+
+int xpos = 20;
+
+// Weird Buzzing when not in servo range
+// Servo Range: 20 - 180
 
 void setup()
 {
-    pinMode(pin, OUTPUT);
     Serial.begin(9600);
-    Serial.println("Serial connection Ready...");
+    Serial.println("Serial Connection Ready...");
+    
+    servoX.attach(9);
+    servoX.write(90); // Set Servo Pos at Middle
+    delay(15);
 }
 
 void loop()
-{
+{   
+    // Only run if receiving new data
     if (Serial.available())
     {
         serialData = Serial.read();
-        Serial.print(serialData);
+        Serial.println(serialData);
 
-        if (serialData == 'L')
+        if (serialData == 'L')  // Turn X Servo Left
         {
-            digitalWrite(pin, HIGH);
+          ++xpos;
+          
+          servoX.write(xpos);
+          delay(15);
         }
-        else if (serialData == 'R')
+        else if (serialData == 'R') // Turn X Servo Right
         {
-            digitalWrite(pin, LOW);
+          --xpos;
+
+          servoX.write(xpos);
+          delay(15);
         }
+
+        Serial.print("Servo Pos: ");
+        Serial.println(xpos);
     }
 }
